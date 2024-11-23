a61c3a9e783ab7ac893cc249ad63532048f08314
Arsenii <137112649+TymArsenii@users.noreply.github.com>
2024-11-23 20:07:24 +0200

    Update Bitmaps.ino

--- Software/.ino/Bitmaps.ino
+++ Software/.ino/Bitmaps.ino
@@ -1,142 +1,203 @@
 uint32_t bitmaps_invert_timer;
 uint32_t bitmaps_change_timer;
 
-uint8_t bitmap_id;
 bool inv;
 
-#define BITMAPS_CHANGE_FREQUENCY 3000
+#define BITMAPS_CHANGE_FREQUENCY 4000
 #define BITMAPS_INVERT_FREQUENCY 300
-#define BITMAPS_AMOUNT 4
+#define BITMAPS_AMOUNT 6
 
-uint8_t heart[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
-    {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+const uint8_t heart[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+  {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
-uint8_t heart_inv[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};
-
-uint8_t smile[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
-    {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+const uint8_t heart_inv[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
+  {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
-uint8_t cube[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-    {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+const uint8_t smile[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+  {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
-uint8_t house[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-    {0, 1, 1, 0, 1, 1, 0, 0, 1, 0},
-    {0, 1, 1, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 1, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+const uint8_t cube[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+const uint8_t checks[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+  {0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+  {0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+  {0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+  {0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+const uint8_t house[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 1, 1, 0, 1, 1, 0, 0, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+const uint8_t ohm[10][10] PROGMEM=
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
 void bitmaps()
 {
-  if (millis() - bitmaps_change_timer >= BITMAPS_CHANGE_FREQUENCY * time_multiplyer)
+  if (millis()-bitmaps_change_timer>=BITMAPS_CHANGE_FREQUENCY*time_multiplyer)
   {
-    bitmaps_change_timer = millis();
+    bitmaps_change_timer=millis();
     bitmap_id++;
-    if (bitmap_id > BITMAPS_AMOUNT)
+    if (bitmap_id>BITMAPS_AMOUNT)
     {
-      bitmap_id = 1;
+      bitmap_id=1;
+      //if(ee_data.auto_effect_change) ee_data.change++;
     }
   }
-  if (millis() - bitmaps_invert_timer >= BITMAPS_INVERT_FREQUENCY)
+  if (millis()-bitmaps_invert_timer>=BITMAPS_INVERT_FREQUENCY)
   {
-    bitmaps_invert_timer = millis();
-    inv = !inv;
+    bitmaps_invert_timer=millis();
+    inv=!inv;
   }
-  //bitmap_id = 1;
+  //bitmap_id=1;
   switch (bitmap_id)
   {
     case 1:
-      for (uint8_t x = 1; x <= 8; x++)
+      for (uint8_t x=1; x<=8; x++)
       {
-        for (uint8_t y = 1; y <= 8; y++)
+        for (uint8_t y=1; y<=8; y++)
         {
           if (inv)
           {
-            matrix_container[x][y] = heart_inv[y][x];
+            matrix_container[x][y]=pgm_read_byte(&heart_inv[y][x]);
           }
           else
           {
-            matrix_container[x][y] = heart[y][x];
+            matrix_container[x][y]=pgm_read_byte(&heart[y][x]);
           }
-          time_multiplyer = 2;
+          time_multiplyer=1;
         }
       }
-      break;
+    break;
     case 2:
-      for (uint8_t x = 1; x <= 8; x++)
+      for (uint8_t x=1; x<=8; x++)
       {
-        for (uint8_t y = 1; y <= 8; y++)
+        for (uint8_t y=1; y<=8; y++)
         {
-          matrix_container[x][y] = smile[y][x];
+          matrix_container[x][y]=pgm_read_byte(&smile[y][x]);
         }
       }
-      time_multiplyer = 1;
-      break;
+      time_multiplyer=1;
+    break;
     case 3:
-      for (uint8_t x = 1; x <= 8; x++)
+      for (uint8_t x=1; x<=8; x++)
       {
-        for (uint8_t y = 1; y <= 8; y++)
+        for (uint8_t y=1; y<=8; y++)
         {
-          matrix_container[x][y] = cube[y][x];
+          matrix_container[x][y]=pgm_read_byte(&cube[y][x]);
         }
       }
-      time_multiplyer = 1;
-      break;
+      time_multiplyer=1;
+    break;
     case 4:
-      for (uint8_t x = 1; x <= 8; x++)
+      for (uint8_t x=1; x<=8; x++)
       {
-        for (uint8_t y = 1; y <= 8; y++)
+        for (uint8_t y=1; y<=8; y++)
         {
-          matrix_container[x][y] = house[y][x];
+          matrix_container[x][y]=pgm_read_byte(&house[y][x]);
         }
       }
-      time_multiplyer = 1;
-      break;
+      time_multiplyer=1;
+    break;
+    case 5:
+      for (uint8_t x=1; x<=8; x++)
+      {
+        for (uint8_t y=1; y<=8; y++)
+        {
+          if (inv)
+          {
+            matrix_container[x][y]=pgm_read_byte(&checks[y][x]);
+          }
+          else
+          {
+            bool value=pgm_read_byte(&checks[y][x]);
+            matrix_container[x][y]=!value;
+          }
+        }
+      }
+      time_multiplyer=3;
+    break;
+    case 6:
+      for (uint8_t x=1; x<=8; x++)
+      {
+        for (uint8_t y=1; y<=8; y++)
+        {
+          matrix_container[x][y]=pgm_read_byte(&ohm[y][x]);
+        }
+      }
+      time_multiplyer=2;
+    break;
   }
 }
